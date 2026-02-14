54673303e95fa31c864b6b7a4e845ca3f135e964
Eric Lundell <emlundell@alaska.edu>
2026-02-13 17:38:15 -0900

    Lint...

--- cluster-cdk/tests/unit/test_cluster_cdk_stack.py
+++ cluster-cdk/tests/unit/test_cluster_cdk_stack.py
@@ -3,12 +3,17 @@ import aws_cdk.assertions as assertions
 
 from cluster_cdk.cluster_cdk_stack import ClusterCdkStack
 
+
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cluster_cdk/cluster_cdk_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = ClusterCdkStack(app, "cluster-cdk")
     template = assertions.Template.from_stack(stack)
+
+    # print so linter is happy
+    print(template)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
