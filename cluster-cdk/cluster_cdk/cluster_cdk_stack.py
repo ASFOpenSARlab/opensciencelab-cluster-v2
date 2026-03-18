242a85344e9a2706adff1eb5c10505b08ca34dfb
Eric Lundell <emlundell@alaska.edu>
2026-03-17 17:34:05 -0800

    Update Checkout version. Add DEPLOY_PREFIX. Simplify workflow_dispatch

--- cluster-cdk/cluster_cdk/cluster_cdk_stack.py
+++ cluster-cdk/cluster_cdk/cluster_cdk_stack.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import (
     Duration,
     Stack,
@@ -12,6 +14,9 @@ from constructs import Construct
 class ClusterCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # Since deploy_prefix is required, intentionally throw an error if not existing.
+        DEPLOY_PREFIX = os.environ["DEPLOY_PREFIX"]
 
         build_role = iam.Role(
             self,
@@ -187,7 +192,7 @@ class ClusterCdkStack(Stack):
                 "hub": {
                     "image": {
                         "name": "ghcr.io/asfopensarlab/opensciencelab-cluster-v2/cluster/jupyterhub",
-                        "tag": "main",
+                        "tag": DEPLOY_PREFIX,
                         "pullPolicy": "Always",
                     },
                     "db": {
