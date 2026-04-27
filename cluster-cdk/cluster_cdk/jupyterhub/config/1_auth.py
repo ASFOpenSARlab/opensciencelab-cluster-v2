75a2151764bf3909243d95ed540f64a3421b3388
Eric Lundell <emlundell@alaska.edu>
2026-04-27 15:00:46 -0800

    Make sure any auth errors lead to null auth

--- cluster-cdk/cluster_cdk/jupyterhub/config/1_auth.py
+++ cluster-cdk/cluster_cdk/jupyterhub/config/1_auth.py
@@ -4,6 +4,8 @@ import boto3
 
 # This try/except is needed for debugging if a problem occurs. AWS Codebuild doesn't allow for useful error messaging.
 try:
+    from jupyterhub.portal_auth import PortalAuthenticator
+
     AWS_REGION = os.environ.get("AWS_REGION", "")
     SSO_TOKEN_ARN = os.environ.get("SSO_TOKEN_ARN", "")
     LAB_PREFIX = os.environ.get("JUPYTERHUB_LAB_PREFIX", "")
@@ -15,21 +17,11 @@ try:
     with open(sso_token_path, "w") as file:
         file.write(_sso_token["SecretString"])
 
-    # If an error occurs with setting the auth but JupyterHub still starts, the dummy login will be the default.
-    # This could lead to unauthorized entry. So disable login until the last needed moment.
-    print("Disabling login temporarily...")
-    c.JupyterHub.authenticator_class = (  # noqa: F821
-        "nullauthenticator.NullAuthenticator"
-    )
-
     c.JupyterHub.default_url = f"{LAB_PREFIX}/hub/home"  # noqa: F821
 
     c.JupyterHub.tornado_settings = {  # noqa: F821
         "cookie_options": {"expires_days": 7.0},
     }
-
-    print("All good so far. Setting login to Portal Auth...")
-    from jupyterhub.portal_auth import PortalAuthenticator
 
     c.JupyterHub.authenticator_class = PortalAuthenticator  # noqa: F821
 
@@ -37,7 +29,14 @@ try:
     c.Authenticator.auth_refresh_age = 60  # noqa: F821
 
 except Exception as e:
-    print(e)
+    print(f"Something went wrong with auth... {e}")
+
+    # If an error occurs with setting the auth but JupyterHub still starts, the dummy login will be the default.
+    # This could lead to unauthorized entry. So disable login until the last needed moment.
+    print("Disabling login temporarily...")
+    c.JupyterHub.authenticator_class = (  # noqa: F821
+        "nullauthenticator.NullAuthenticator"
+    )
 
 finally:
     print("Done with extraConfig::auth.py")
