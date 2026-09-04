457656aaf25288619c33779f19b5480cb4126f82
Dylan Palmieri <37966424+dgpalmieri@users.noreply.github.com>
2026-09-04 14:36:27 -0800

    fix: add two new public subnets

--- cluster-cdk/cluster_cdk/cluster_cdk_stack.py
+++ cluster-cdk/cluster_cdk/cluster_cdk_stack.py
@@ -132,25 +132,13 @@ class ClusterCdkStack(Stack):
         #    Setup Networking
         #
         #####################################################################
-
         # Two subnets for EKS
-        public_subnet = ec2.SubnetConfiguration(
-            name="PublicSubnet",
-            subnet_type=ec2.SubnetType.PUBLIC,
-            cidr_mask=24,
-        )
-        print(public_subnet)
         private_subnet = ec2.SubnetConfiguration(
             name="PrivateSubnetWithEgress",
             subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
             cidr_mask=24,
         )
 
-        larger_public_subnet = ec2.SubnetConfiguration(
-            name="LargerPublicSubnet",
-            subnet_type=ec2.SubnetType.PUBLIC,
-            cidr_mask=20,
-        )
         # Create a custom VPC restricted to the single AZ
         self.vpc = ec2.Vpc(
             self,
@@ -158,7 +146,25 @@ class ClusterCdkStack(Stack):
             availability_zones=[f"{self.region}{self.AZ_LETTER}", f"{self.region}d"],
             ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
             # Configure subnet types for EKS (e.g., Public and Private)
-            subnet_configuration=[larger_public_subnet, private_subnet],
+            subnet_configuration=[private_subnet],
+        )
+
+        # Create explicit public subnet with an exact CIDR 
+        public_subnet_1 = ec2.CfnSubnet(
+            self, 
+            "PublicSubnet1",
+            vpc_id=self.vpc,
+            cidr_block="10.0.250.0/20",
+            availability_zone=f"{self.region}{self.AZ_LETTER}",
+            map_public_ip_on_launch=True
+        )        
+        public_subnet_2 = ec2.CfnSubnet(
+            self, 
+            "PublicSubnet2",
+            vpc_id=self.vpc,
+            cidr_block="10.0.125.0/20",
+            availability_zone=f"{self.region}d",
+            map_public_ip_on_launch=True
         )
 
         #####################################################################
